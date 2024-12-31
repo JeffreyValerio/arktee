89fc2d2a84eb1971f435ee9a10c22d655690cb7e
JeffreyValerio <jeffreyvalerio@hotmail.com>
2024-12-31 08:39:07 -0600

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "./globals.css";
-import { Footer, Header, Sidebar } from "@/components";
+import { Footer, Header } from "@/components";
 import { Suspense } from "react";
 
 const montserrat = localFont({
