50812b92668424f8d5dd8cb622d6bd3b76c08615
JeffreyValerio <jeffreyvalerio@hotmail.com>
2025-01-01 22:27:55 -0600

    mejoras

--- src/components/products/NoResults.tsx
+++ src/components/products/NoResults.tsx
@@ -27,8 +27,8 @@ export const NoResults = async ({ params }: { params: { query: string } }) => {
  
             <div className="mb-8 text-sm">
                 <p className='flex items-center gap-2 mb-4 font-bold'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-info"><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg>
-                    Recomendaciones :
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-info"><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg>
+                    Recomendaciones:
                 </p> 
                 <ul className='pl-4 list-disc'>
                     <li>Verifica la ortografía de tu palabra clave.</li>
@@ -38,7 +38,7 @@ export const NoResults = async ({ params }: { params: { query: string } }) => {
                 </ul>
             </div>
 
-            <div className="mb-8">
+            <div className="mb-4 md:mb-8">
                 <p className='text-xl'>Te podría interesar</p>
             </div>
 
